b6ee77937afeaeb7df102221cad38f496076cc68
AbdullahbinTahir42 <abdullahbintahir42@gmail.com>
2025-02-19 21:31:28 +0500

    2nd

---
+++ palidrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        new = 0
+        old = x
+        while x > 0:
+            r = x % 10
+            new = (new * 10) + r
+            x = x // 10
+
+        if old == new:
+            return True
+        return False
+
+
+        
