a3d0c1a8fc993ebfaecef41c76e44d2106202e75
Abdullah Bin Tahir <abdullahbintahir42@gmail.com>
2026-01-27 00:03:07 +0500

    move zeros

---
+++ move_zeros.py
@@ -0,0 +1,20 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        p1,p2 = 0,1
+        while(p2 < len(nums)):
+            if nums[p1] == 0 and nums[p2] != 0:
+                nums[p1],nums[p2] = nums[p2],nums[p1]
+                p1 += 1
+                p2 += 1
+            elif nums[p1] == 0 and nums[p2] == 0:
+                p2 += 1
+            else:
+                p1 += 1
+                p2 += 1
+        
+        return nums
+
+            
